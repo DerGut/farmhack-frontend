9646c47404db521cb238f8af5b8050b80c6dbbb3
DerGut <jannik.steinmann@gmx.de>
2019-10-17 10:36:34 +0200

    Add location data from s3

--- js/map.js
+++ js/map.js
@@ -5,104 +5,109 @@ L.tileLayer(tileServerUrl, {
     id: 'mapbox.light'
 }).addTo(map);
 
-L.geoJson(statesData).addTo(map);
+var xhttp = new XMLHttpRequest();
+xhttp.onload = function() {
+    var data = JSON.parse(this.responseText);
 
-function getColor(d) {
-    return d > 1000 ? '#800026' :
-        d > 500  ? '#BD0026' :
-        d > 200  ? '#E31A1C' :
-        d > 100  ? '#FC4E2A' :
-        d > 50   ? '#FD8D3C' :
-        d > 20   ? '#FEB24C' :
-        d > 10   ? '#FED976' :
-        '#FFEDA0';
-}
+    function getColor(d) {
+        return d > 14 ? '#800026' :
+            d > 12  ? '#BD0026' :
+                d > 10  ? '#E31A1C' :
+                    d > 8  ? '#FC4E2A' :
+                        d > 6   ? '#FD8D3C' :
+                            d > 4   ? '#FEB24C' :
+                                d > 2   ? '#FED976' :
+                                    '#FFEDA0';
+    }
 
-function style(feature) {
-    return {
-        fillColor: getColor(feature.properties.density),
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.7
+    function style(feature) {
+        return {
+            fillColor: getColor(feature.properties.KULTURCODE),
+            weight: 2,
+            opacity: 1,
+            color: 'white',
+            dashArray: '3',
+            fillOpacity: 0.7
+        };
+    }
+
+    var geojson;
+
+    function highlightFeature(e) {
+        var layer = e.target;
+
+        layer.setStyle({
+            weight: 5,
+            color: '#666',
+            dashArray: '',
+            fillOpacity: 0.7
+        });
+
+        if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+            layer.bringToFront();
+        }
+
+        info.update(layer.feature.properties);
+    }
+
+    function resetHighlight(e) {
+        geojson.resetStyle(e.target);
+        info.update();
+    }
+
+    function zoomToFeature(e) {
+        map.fitBounds(e.target.getBounds());
+    }
+
+    function onEachFeature(feature, layer) {
+        layer.on({
+            mouseover: highlightFeature,
+            mouseout: resetHighlight,
+            click: zoomToFeature
+        });
+    }
+
+    geojson = L.geoJson(data, {
+        style: style,
+        onEachFeature: onEachFeature
+    }).addTo(map);
+
+    var info = L.control();
+
+    info.onAdd = function (map) {
+        this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+        this.update();
+        return this._div;
     };
-}
 
-var geojson;
+    // method that we will use to update the control based on feature properties passed
+    info.update = function (props) {
+        this._div.innerHTML = '<h4>Kulturcode</h4>' +  (props ?
+            '<b>' + props.FLIK + '</b><br />' + props.KULTURCODE : 'Hover over a Feld');
+    };
 
-function highlightFeature(e) {
-    var layer = e.target;
+    info.addTo(map);
 
-    layer.setStyle({
-        weight: 5,
-        color: '#666',
-        dashArray: '',
-        fillOpacity: 0.7
-    });
+    var legend = L.control({position: 'bottomright'});
 
-    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-        layer.bringToFront();
-    }
+    legend.onAdd = function (map) {
 
-    info.update(layer.feature.properties);
-}
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades = [0, 2, 4, 6, 8, 10, 12, 14],
+            labels = [];
 
-function resetHighlight(e) {
-    geojson.resetStyle(e.target);
-    info.update();
-}
+        // loop through our density intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
 
-function zoomToFeature(e) {
-    map.fitBounds(e.target.getBounds());
-}
+        return div;
+    };
 
-function onEachFeature(feature, layer) {
-    layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight,
-        click: zoomToFeature
-    });
-}
+    legend.addTo(map);
 
-geojson = L.geoJson(statesData, {
-    style: style,
-    onEachFeature: onEachFeature
-}).addTo(map);
-
-var info = L.control();
-
-info.onAdd = function (map) {
-    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
-    this.update();
-    return this._div;
 };
-
-// method that we will use to update the control based on feature properties passed
-info.update = function (props) {
-    this._div.innerHTML = '<h4>Irgendwas mit Pflanzen</h4>' +  (props ?
-        '<b>' + props.name + '</b><br />' + props.density + ' Feldkanten / Parsec<sup>2</sup>'
-        : 'Hover over a Feld');
-};
-
-info.addTo(map);
-
-var legend = L.control({position: 'bottomright'});
-
-legend.onAdd = function (map) {
-
-    var div = L.DomUtil.create('div', 'info legend'),
-        grades = [0, 10, 20, 50, 100, 200, 500, 1000],
-        labels = [];
-
-    // loop through our density intervals and generate a label with a colored square for each interval
-    for (var i = 0; i < grades.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-    }
-
-    return div;
-};
-
-legend.addTo(map);
+xhttp.open("GET", "https://farmhack-os.s3.eu-central-1.amazonaws.com/data.geojson", true);
+xhttp.send();
