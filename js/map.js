77e62782e9ef04ebffe49f0ae8bd606d24ca7a5a
DerGut <jannik.steinmann@gmx.de>
2019-10-17 11:23:27 +0200

    Add labels

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([52.5545,7.9015], 11);
+var map = L.map('map').setView([52.5545,7.9015], 13);
 var accessToken = 'pk.eyJ1IjoiZGVyZ3V0IiwiYSI6ImNpdG5lMmE1bDAwMzQzMm83NHFpaTdlaW8ifQ.hp122jysRM5Ic7TziyYFow';
 var tileServerUrl = 'https://api.mapbox.com/styles/v1/dergut/ck1tif3rb64un1cn6s2gszr90/tiles/256/{z}/{x}/{y}?access_token=' + accessToken;
 L.tileLayer(tileServerUrl, {
@@ -10,13 +10,13 @@ xhttp.onload = function() {
     var data = JSON.parse(this.responseText);
 
     function getColor(d) {
-        return d > 14 ? '#800026' :
-            d > 12  ? '#BD0026' :
-                d > 10  ? '#E31A1C' :
-                    d > 8  ? '#FC4E2A' :
-                        d > 6   ? '#FD8D3C' :
-                            d > 4   ? '#FEB24C' :
-                                d > 2   ? '#FED976' :
+        return d > 6 ? '#800026' :
+            d > 5  ? '#BD0026' :
+                d > 4  ? '#E31A1C' :
+                    d > 3  ? '#FC4E2A' :
+                        d > 2   ? '#FD8D3C' :
+                            d > 1   ? '#FEB24C' :
+                                d > 0   ? '#FED976' :
                                     '#FFEDA0';
     }
 
@@ -80,10 +80,44 @@ xhttp.onload = function() {
         return this._div;
     };
 
+    var cultures = {
+        0: "Winterhartweizen",
+        1: "Sommerhartweizen",
+        2: "Winterroggen",
+        3: "Winterroggen",
+        4: "Sommerroggen",
+        5: "Wintergerste",
+        6: "Sommergerste",
+        7: "Winterhafer",
+        8: "Sommerhafer",
+        9: "Wintertriticale",
+        10: "Sommertriticale",
+        11: "Körnermais",
+        12: "Rispenhirse",
+        13: "Buchweizen",
+        14: "Amarant",
+        // 15:
+        // 16: ""
+        // 17
+        18: "Erbsen",
+        19: "Ackerbohnen",
+        20: "Lupinen",
+        // 21:
+        22: "Linsen",
+        // 23:
+        // 24:
+        25: "Winterraps",
+        26: "Sommerraps",
+        27: "Winterrübsen",
+        28: "Winterrübsen",
+        29: "Sonnenblumen",
+
+    };
+
     // method that we will use to update the control based on feature properties passed
     info.update = function (props) {
-        this._div.innerHTML = '<h4>Kulturcode</h4>' +  (props ?
-            '<b>' + props.FLIK + '</b><br />' + props.KULTURCODE : 'Hover over a Feld');
+        this._div.innerHTML = '<h4>Kultur</h4>' +  (props ?
+            '<b>' + cultures[props.KULTURCODE] + '</b>' : 'Hover über a Feld');
     };
 
     info.addTo(map);
@@ -93,14 +127,11 @@ xhttp.onload = function() {
     legend.onAdd = function (map) {
 
         var div = L.DomUtil.create('div', 'info legend'),
-            grades = [0, 2, 4, 6, 8, 10, 12, 14],
-            labels = [];
+            grades = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
         // loop through our density intervals and generate a label with a colored square for each interval
         for (var i = 0; i < grades.length; i++) {
-            div.innerHTML +=
-                '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+            div.innerHTML += '<i style="background:' + getColor(i) + '"></i> ' + cultures[i] + '<br>' ;
         }
 
         return div;
